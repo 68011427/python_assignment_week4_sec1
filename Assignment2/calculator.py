8caaaad96d200d5589ae5e845ea3d74faa244195
Pakorn <68011427@kmitl.ac.th>
2026-07-22 11:00:38 +0700

    Add basic arithmetic operations in calculator and lambda_calculator; create string_operations file

---
+++ Assignment2/calculator.py
@@ -0,0 +1,19 @@
+a = 2
+b =3
+
+def add(a,b):
+    return a+ b
+
+def subtract(a,b):
+    return a -b
+
+def mutiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+print("Add:", add(a,b))
+print("Subtract:", subtract(a,b))
+print("Multiply:", mutiply(a,b))
+print("Divide:", divide(a,b))
---
+++ Assignment3/lambda_calculator.py
@@ -0,0 +1,12 @@
+a = 2
+b = 3
+
+add = lambda a,b: a + b 
+subtraction  = lambda a,b: a-b
+multiplication = lambda a,b: a*b
+division = lambda a,b: a/b
+
+print("Add:", add(a,b))
+print("Subtract:", subtraction(a,b))
+print("Multiply:", multiplication(a,b))
+print("Divide:", division(a,b))
